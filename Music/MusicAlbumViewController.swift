72360c3420ec411d9e471535b1eb1f3f9b149cb8
Семён Беляков <semenbelakov@MacBook-Air-Semen.local>
2023-08-30 19:45:19 +0500

    first commit

---
+++ Music/MusicAlbumViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MusicAlbumViewController.swift
+//  Music
+//
+//  Created by Семён Беляков on 30.08.2023.
+//
+
+import UIKit
+
+class MusicAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
